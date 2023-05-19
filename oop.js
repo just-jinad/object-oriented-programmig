51994084a1b30b205ca350bae61e9058a6b95340
isaac jinad <jinadtope66@gmail.com>
2023-05-19 11:00:09 +0100

    oop

---
+++ oop.js
@@ -0,0 +1,39 @@
+class Student {
+    constructor(name,age,gender,dept){
+        this.name = name
+        this.age = age
+        this.gender = gender
+        this.dept = dept
+    }
+}
+
+student1 = new Student("jinad Tope","4","Male","Software engineering")
+console.log(student1)
+
+student2 = new Student("jinad Isaac","6","Male","Robotics")
+console.log(student2)
+
+student3 = new Student("Ade sola","21","Female","UI/UX")
+console.log(student3)
+
+student4 = new Student("Taylor jade","21","Female","UI/UX")
+console.log(student4)
+
+student5 = new Student("Samuel jacob","20","Male","Software engineering")
+console.log(student5)
+
+student6 = new Student("daniel kunle","21","Male","Cyber Security")
+console.log(student6)
+
+student7 = new Student( "Somisola Fred","19","Female","Data science")
+console.log(student7)
+
+student8 = new Student("Testimony Richard","14","Female","UI/UX")
+console.log(student8)
+
+
+student9 = new Student("Olamide Alao","24","Male","Robotics")
+console.log(student9)
+
+student10 = new Student("Hannah Micheal","24","Female","Data science")
+console.log(student10)
